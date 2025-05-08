cae5d1c39cc0ef24d9a6256b56de0eeb2413a94c
haxibro <hash936.naver.com>
2025-05-08 10:40:15 +0900

    Debug: index.html 위치 변경 src 안으로

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // 빌드된 파일들이 저장될 폴더
+    assetsDir: 'assets',  // 자산 파일들이 저장될 폴더
+  },
+});
