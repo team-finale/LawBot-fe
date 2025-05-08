afd1808d626811ea9a25edfa2a3f2c567120372a
haxibro <hash936.naver.com>
2025-05-08 13:42:48 +0900

    Debug: build 성공

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // 빌드된 파일들이 저장될 폴더
-    assetsDir: 'assets',  // 자산 파일들이 저장될 폴더
+    outDir: 'dist',      // 빌드된 파일들이 저장될 폴더
+    assetsDir: 'assets', // 자산 파일들이 저장될 폴더
   },
+  base: '/',  // 루트 경로에서 배포하려면 base를 빈 문자열로 설정
 });
