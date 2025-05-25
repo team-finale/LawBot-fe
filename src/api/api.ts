d3e4b34ddcec160447d87df5d642e03bd2fa6c1f
haxibro <hash936.naver.com>
2025-05-26 03:36:47 +0900

    Debug: https 설정(주석처리)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://2lawon.com/api", // API 주소
@@ -12,4 +12,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default api;*/
