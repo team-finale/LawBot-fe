ca77403d85f194440014bd42e725cb29309d5565
haxibro <hash936.naver.com>
2025-09-27 18:42:59 +0900

    퀴즈 기능 수정

--- src/pages/LawyerImageUpload.tsx
+++ src/pages/LawyerImageUpload.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "./quiz.css";
+import "./Quiz.css";
 
 import { useMemo, useState } from "react";
 import axios from "axios";
