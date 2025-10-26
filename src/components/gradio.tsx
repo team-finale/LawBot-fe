6ab8719749828c6bc721febf0bdabcef63455c50
haxibro <hash936.naver.com>
2025-10-26 23:11:33 +0900

    debug: gradio

--- src/components/gradio.tsx
+++ src/components/gradio.tsx
@@ -7,7 +7,7 @@ const Gradio = () => {
     <section id="gradio" className="gradio-section">
       <h1 className="gradio-title">이로운에게 물어보세요</h1>
       <p className="gradio-subtitle">LLM+RAG 기반 챗봇이 빠르고 쉽게 알려드릴게요</p>
-       <a href="/consult" className="gradio-button">이로운에게 물어보기</a>
+       <a href="/gradio" className="gradio-button">이로운에게 물어보기</a>
     </section>
     
   );
