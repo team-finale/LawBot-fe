a49aa4c3c875748b8dd35fc4c0a7d2ca3c693e25
haxibro <hash936.naver.com>
2025-10-28 15:43:34 +0900

    버그

--- src/components/Scenario.tsx
+++ src/components/Scenario.tsx
@@ -8,7 +8,7 @@ const Scenario = () => {
   return (
     <section id="scenarios" className="scenarios-section">
    
-      <p className="scenarios-subtitle">실제 상황을 기반으로 한 문제를 풀어보세요</p>
+      <p className="scenarios-subtitle">실제 상황을 기반으로 한 문제를 풀어보세요!!</p>
       <button onClick={() => navigate("/scenarios")} className="scenarios-button">
         시나리오 퀴즈 풀기
       </button>
