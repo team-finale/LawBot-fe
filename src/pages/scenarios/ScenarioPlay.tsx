29ddbe431477c15262c5646d428b0530612701ce
haxibro <hash936.naver.com>
2025-10-28 16:49:47 +0900

    수정2

--- src/pages/scenarios/ScenarioPlay.tsx
+++ src/pages/scenarios/ScenarioPlay.tsx
@@ -1,5 +1,5 @@
 // src/pages/scenarios/ScenarioPlay.tsx
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
